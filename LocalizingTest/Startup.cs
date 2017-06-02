ea49d37de9dff6ca352887ae620ad999d52c3769
David Keller <git@dkbe.ch>
2017-06-02 17:53:19 +0200

    Init

---
+++ LocalizingTest/Startup.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Razor;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.Extensions.Options;
+
+namespace LocalizingTest
+{
+    public class Startup
+    {
+        public static string CULTURE_COOKIE_KEY = "culture-test";
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddLocalization(locSetup => locSetup.ResourcesPath = "Resources");
+
+            var supportedCultures = new CultureInfo[]
+            {
+                //new CultureInfo("de"),
+                new CultureInfo("de-DE"),
+                new CultureInfo("en-GB"),
+                //new CultureInfo("en"),
+                //new CultureInfo("en-US"),
+            };
+
+            services.Configure<RequestLocalizationOptions>(s =>
+            {
+                s.SupportedCultures = supportedCultures;
+                s.SupportedUICultures = supportedCultures;
+                s.DefaultRequestCulture = new RequestCulture(culture: "en-GB", uiCulture: "en-GB");
+                s.RequestCultureProviders.Clear();
+                s.RequestCultureProviders.Insert(0, new QueryStringRequestCultureProvider());
+                s.RequestCultureProviders.Insert(1, new CookieRequestCultureProvider() { CookieName = CULTURE_COOKIE_KEY });
+                s.RequestCultureProviders.Insert(2, new AcceptLanguageHeaderRequestCultureProvider());
+            });
+
+
+            services
+                .AddMvc()
+                .AddViewLocalization(LanguageViewLocationExpanderFormat.Suffix, s => s.ResourcesPath = "Resources")
+                .AddDataAnnotationsLocalization();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            var locOptions = app.ApplicationServices.GetService<IOptions<RequestLocalizationOptions>>();
+            app.UseRequestLocalization(locOptions.Value);
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
